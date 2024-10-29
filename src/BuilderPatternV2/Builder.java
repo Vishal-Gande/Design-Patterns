b3b9a681849b4eaa36d0f1fa0e5459ec40a6abde
Vishal-Gande <vishalchinnaa@gmail.com>
2024-10-29 16:39:30 +0530

    Added builder pattern Version 2

---
+++ src/BuilderPatternV2/Builder.java
@@ -0,0 +1,35 @@
+package BuilderPatternV2;
+
+public class Builder {
+
+
+    // public coz these attrs will be accessed in Student constructor
+    public String name;
+    public int age;
+    public double gpa;
+
+
+    public Builder setName(String name) {
+        this.name = name;
+        return this;
+    }
+    public Builder setAge(int age) {
+        this.age = age;
+        return this;
+    }
+    public Builder setGpa(double gpa) {
+        this.gpa = gpa;
+        return this;
+    }
+
+    public Student build(){
+
+        // add code for validation and throw exceptions
+        return new Student(this);
+    }
+    public boolean validateAttrs()
+    {
+        // add logic for false
+        return true;
+    }
+}
---
+++ src/BuilderPatternV2/Student.java
@@ -0,0 +1,24 @@
+package BuilderPatternV2;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private double gpa;
+
+
+    public Student(Builder b)
+    {
+        this.name = b.name;
+        this.age = b.age;
+        this.gpa = b.gpa;
+
+        System.out.println("Student object created successfully");
+    }
+
+    // has to be static as you're calling this class variable in client line no. 8
+    public static Builder getBuilder()
+    {
+        return new Builder();
+    }
+}
